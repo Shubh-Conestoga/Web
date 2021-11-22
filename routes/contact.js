6cf882a6cdd2ee530f5cb20f26a9dec8c7a3cc21
Shubh-Conestoga <Spatel8035@conestogac.on.ca>
2021-11-22 16:24:34 +0530

    final

---
+++ routes/contact.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+
+router.post('/submit',(req,res,next)=>{
+    var {email} = req.body;
+    var {message} = req.body;
+    var isSuccess = true;
+    if(email==="" || message==="")
+    {
+        isSuccess=false
+    }
+    console.log(email , message);
+    res.render('contact',{
+        "Message":message,
+        "Email":email,
+        "success":isSuccess,
+        title:"Contact",
+        "core":false,
+    })
+});
+
+
+router.get('/',(req,res,next)=>{
+    
+    res.render('contact',{
+        title:"Contact",
+        "core":true,
+    })
+});
+
+module.exports = router;
